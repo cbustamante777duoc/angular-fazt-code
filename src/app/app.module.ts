9e5ff47bddfcfe3de1a9c0ae6da712582909922a
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-21 15:04:16 -0300

    primeros pasos con angular uno de input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWord } from './primerComponente/primer.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWord,
+    HolaMundoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/primerComponente/primer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  templateUrl: './primer.component.html',
+  styleUrls: ['./primer.component.css']
+
+
+})
+
+export class HelloWord {
+
+  title = 'primera vista de angular';
+  nombre = 'lalisa manoban'
+}
