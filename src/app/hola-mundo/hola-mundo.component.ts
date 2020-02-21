b28f63450f405e97c026913e1972b5e413a2440c
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-21 15:44:06 -0300

    agrendo un personaje al hola-mundo.componet

--- src/app/hola-mundo/hola-mundo.component.ts
+++ src/app/hola-mundo/hola-mundo.component.ts
@@ -10,8 +10,22 @@ export class HolaMundoComponent implements OnInit {
   titulo = 'segundo componente';
   users = ['lalisa', 'jennie kim', 'rose', 'jisoo'];
   activated = true;
+  name = 'millie';
+  age: number;
+  adress: {
+    street: string,
+    city: string
+  };
+  hobbies: string [];
 
-  constructor() { }
+  constructor() {
+    this.age = 16;
+    this.adress = {
+      street: 'hollywood',
+      city: 'california'
+    };
+    this.hobbies = ['sing', 'dance', 'performance'];
+   }
 
   ngOnInit(): void {
   }
